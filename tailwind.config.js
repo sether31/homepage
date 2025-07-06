6f8b2e5689aafe1f3d1d50a39bf6ecac3dececfd
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-07-06 15:39:39 +0800

    feat: Finish about me section
    - Create own animation.
    - Add images.
    - Create container component.
    - Finish about me on homepage and is responsive.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./public/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        lb: {
+          '0%': { transform: 'skewY(0deg)' },
+          '80%': { transform: 'skewY(-5deg)' },
+          '100%': { transform: 'skewY(-10deg)' },
+        }
+      },
+      animation: {
+        lb: 'lb 1.3s ease-in-out forwards'
+      }
+    },
   },
   plugins: [],
 }
