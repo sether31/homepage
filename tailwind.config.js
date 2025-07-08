8439aa9712c76b60af7ad1ed25797619daaa8948
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-07-08 13:17:19 +0800

    chore: Update file path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.{html,js}"
+    "./public/**/*.{html,js}",
+    "./index.html"
   ],
   theme: {
     extend: {
